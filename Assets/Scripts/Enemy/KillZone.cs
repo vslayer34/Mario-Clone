254817850a628cbde9c7bff7edc638d27c00a7aa
vslayer34 <mohamd.elnagar.34@gmail.com>
2022-11-30 20:37:00 +0200

    Added upward force to the player when it hits an enemy

--- Assets/Scripts/Enemy/KillZone.cs
+++ Assets/Scripts/Enemy/KillZone.cs
@@ -16,6 +16,13 @@ public class KillZone : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D player)
     {
         if (player.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D rb = player.gameObject.GetComponent<Rigidbody2D>();
+
             enemy.Die();
+
+            rb.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+
+        }
     }
 }
