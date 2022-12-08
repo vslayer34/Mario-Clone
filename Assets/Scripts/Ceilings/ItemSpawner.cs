da5dc2c09b35c3489c9ded311f3ebe79821ff6dc
vslayer34 <mohamd.elnagar.34@gmail.com>
2022-12-08 04:01:19 +0200

    Managed to implement the spawn function
     - A new item spawn
     - it moves up so the player can grab it

--- Assets/Scripts/Ceilings/ItemSpawner.cs
+++ Assets/Scripts/Ceilings/ItemSpawner.cs
@@ -8,18 +8,31 @@ public class ItemSpawner : MonoBehaviour
     bool isCoin;
     bool isDoubleUp;
 
+    bool itemSpawned;
+
     [SerializeField] GameObject itemPlaceHolder;
+    [SerializeField] Transform itemTransform;
+
+    float speed = 1.0f;
 
     private void Start()
     {
         GenerateAnItem(items[0]);
     }
 
+    private void Update()
+    {
+        if (itemSpawned && itemTransform.localPosition.y < 0.5)
+            itemTransform.Translate(0, speed * Time.deltaTime, 0);
+    }
 
     void GenerateAnItem(Item desiredItem)
     {
-        Instantiate(itemPlaceHolder);
-        itemPlaceHolder.GetComponent<GameObjectData>().item = desiredItem;
+        GameObject newItem;
+        newItem = Instantiate(itemPlaceHolder, itemTransform.position, itemPlaceHolder.transform.rotation);
+        newItem.transform.parent = itemTransform;
+        newItem.GetComponent<GameObjectData>().item = desiredItem;
+        itemSpawned = true;
     }
 
     //void SpawnItem(Item )
